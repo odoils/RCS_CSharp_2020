4b6d6dac52d1b743aec49967232b98a784621cd3
odoils <60353433+odoils@users.noreply.github.com>
2020-01-28 23:33:46 -0800

    Day 27.02

---
+++ Day1_basics/Day1_basics/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Day1_basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //parasta izvade
+            Console.WriteLine("Hello World!");
+            //int-vesels skaitlis
+            //Double,float-skaitlis ar komatu
+            //String- simbolu virkne
+            //char-simbols
+            //bool (boolean)-true vai false
+
+            String piemers = "Piemera teikums";
+            Console.WriteLine(piemers);
+
+            piemers = Console.ReadLine();
+            Console.WriteLine(piemers);
+        }
+    }
+}
