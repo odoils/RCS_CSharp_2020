1288b9ab7232a375d5ce55a4848e1693bbc6b102
odoils <60353433+odoils@users.noreply.github.com>
2020-03-20 06:27:55 -0700

    MD_Random

---
+++ MD Random/MD Random/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace MD_Random
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ierakstiet skaitli no 1 ldz 5");
+            String ievade = Console.ReadLine();
+            int a = Convert.ToInt32(ievade);
+
+
+
+
+
+            /*if (a == 1)
+            {
+                Console.WriteLine("Pirmdiena");
+            }
+            else if (a == 2) 
+            {
+                Console.WriteLine("Otrdiena");
+            }
+            else if (a == 3)
+            {
+                Console.WriteLine("Tresdiena");
+            }
+            else if (a == 4)
+            {
+                Console.WriteLine("Ceturdiena");
+            }
+            else if (a == 5)
+            {
+                Console.WriteLine("Piekdiena");
+            }
+            else
+            {
+                Console.WriteLine("nepareizi, jaievada no 1 lidz 5");
+            }*/
+
+            /*Random rand = new Random();
+
+            for (int i = 0; i < a; i++) ;
+            {
+                Console.WriteLine(rand.Next(1,7));
+            }*/
+        }
+    }
+}
