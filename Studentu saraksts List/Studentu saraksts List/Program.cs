9a2f830f018a5b00692ac0ed4fb4a5afa4d558d7
odoils <60353433+odoils@users.noreply.github.com>
2020-03-20 06:28:35 -0700

    Studentu_saraksts_List

---
+++ Studentu saraksts List/Studentu saraksts List/Program.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Studentu_saraksts_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TaskHomeWork();
+        }
+
+        public static void PrintMenu()
+
+        {
+            Console.WriteLine("1. Izvadit");
+            Console.WriteLine("2. Pievienot jaunu studentu");
+            Console.WriteLine("3. Rediget informaciju");
+            Console.WriteLine("4. Dzest ierakstu");
+            Console.WriteLine("0. Iziet");
+        }
+        static void TaskHomeWork()
+        {
+            List<Students> studenti = new List<Students>();
+            String choice = "";
+
+            while (choice != "0")
+            {
+                PrintMenu();
+                choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        PrintStudents(studenti);
+                        break;
+
+                    case "2":
+                        AddStudent(studenti);
+                        break;
+
+                    case "3":
+                        EditStudent(studenti);
+                        break;
+
+                    case "4":
+                        RemoveStudent(studenti);
+                        break;
+
+                    case "0":
+                        break;
+
+                    default:
+                        Console.WriteLine("Ievadiet 1,2,3,4 vai 0");
+                        break;
+                }
+            }
+        }
+        static void PrintStudents(List<Students> st)
+        {
+            foreach (Students s in st)
+            {
+                s.print();
+            }
+        }
+
+        static void AddStudent(List<Students> st)
+        {
+            Console.WriteLine("Ievadiet vardu");
+            String name = Console.ReadLine();
+
+            Console.WriteLine("Ievadiet uzvardu");
+            String surname = Console.ReadLine();
+
+            Console.WriteLine("Ievadiet kursu");
+
+            try
+            {
+                int course = Convert.ToInt32(Console.ReadLine());
+                st.Add(new Students(name, surname, course));
+            }
+            catch
+            {
+                Console.WriteLine("Nepareiza ievade");
+            }
+        }
+
+        static void EditStudent(List<Students> st)
+        {
+            PrintStudents(st);
+            Console.WriteLine("Kuru ierakstu velaties rediget?");
+            int index = Convert.ToInt32(Console.ReadLine());
+
+            if (index - 1 > st.Count - 1 || index - 1 < 0 || st[index - 1] == null)
+            {
+                Console.WriteLine("Tads ieraksts neeksiste");
+                return;
+            }
+
+            Console.WriteLine("Ko velaties rediget?");
+            Console.WriteLine("1. Vards");
+            Console.WriteLine("2. Uzvards");
+            Console.WriteLine("3. Kurss");
+
+            String choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    Console.WriteLine("Ievadiet vardu!");
+                    st[index - 1].SetName(Console.ReadLine());
+                    break;
+
+                case "2":
+                    Console.WriteLine("Ievadiet uzvrdu!");
+                    st[index - 1].SetSurname(Console.ReadLine());
+                    break;
+
+                case "3":
+                    Console.WriteLine("Ievadiet kursu!");
+
+                    try
+                    {
+                        st[index - 1].SetCourse(Convert.ToInt32(Console.ReadLine()));
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Nepareiza ievade");
+                    }
+
+                    break;
+
+                default:
+                    Console.WriteLine("Tads lauks neeksiste");
+                    break;
+            }
+        }
+
+        static void RemoveStudent(List<Students> st)
+        {
+            PrintStudents(st);
+            Console.WriteLine("Kuru ierakstu velaties rediget?");
+            int index = Convert.ToInt32(Console.ReadLine());
+
+            if (index - 1 > st.Count - 1 || index - 1 < 0 || st[index - 1] == null)
+            {
+                Console.WriteLine("Tads ieraksts neeksiste");
+                return;
+            }
+
+            st.RemoveAt(index - 1);
+            Console.WriteLine("Ieraksts dzests!");
+        }
+    }
+}
+
---
+++ Studentu saraksts List/Studentu saraksts List/Students.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Studentu_saraksts_List
+{
+    class Students
+    {
+        private String name;
+        private String surname;
+        private int course;
+
+        public Students(String name, String surname, int course)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.course = course;
+        }
+        public String GetName()
+        {
+            return name;
+        }
+        public void SetName(String name)
+        {
+            this.name = name;
+        }
+        public String GetSurname()
+        {
+            return surname;
+        }
+        public void SetSurname(String surname)
+        {
+            this.surname = surname;
+        }
+
+        public int GetCourse()
+        {
+            return course;
+        }
+
+        public void SetCourse(int course)
+        {
+            this.course = course;
+        }
+
+        public void print()
+        {
+            Console.WriteLine(name + " " + surname + " " + course);
+        }
+
+
+    }
+}
