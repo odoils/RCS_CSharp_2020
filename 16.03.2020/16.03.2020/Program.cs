25524ad4b056ba5c413e41145461fa261c15c5da
odoils <60353433+odoils@users.noreply.github.com>
2020-03-20 06:14:59 -0700

    16.03.2020

---
+++ 16.03.2020/16.03.2020/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _16._03._2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
